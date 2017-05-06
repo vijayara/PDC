22013478ab00946ac0a276b775bf6bd16a75de48
Thity <thierry.bossy@romandie.com>
2017-05-06 18:20:54 +0200

    modularization next level

--- image_creation.py
+++ image_creation.py
@@ -1,3 +1,4 @@
+import math
 import pygame
 from pygame.locals import *
 
@@ -9,15 +10,25 @@ def camstream():
     #total screen size
     SIZE = (pygame.display.Info().current_w, pygame.display.Info().current_h)
     #separating cross width
-    CROSS_SIZE = 50
+    CROSS_SIZE = 30
     QUADRANT_SIZE = ((SIZE[0]-CROSS_SIZE)/2, (SIZE[1]-CROSS_SIZE)/2)
     #quadrant offsets
     W_OFFSET = QUADRANT_SIZE[0] + CROSS_SIZE
     H_OFFSET = QUADRANT_SIZE[1] + CROSS_SIZE
     
+    #number of lines and columns per quadrant
+    LINE = 3
+    COLUMN = 3
+    
     #number of possible levels for each color
     N_TONS = 2
     TON = [(i)*255/(N_TONS-1) for i in range(0, N_TONS)]
+
+    #number of colors
+    N_COLORS = N_TONS**3
+    
+    COMBINATIONS = N_TONS**(3*LINE*COLUMN)
+    BITS_BY_QUADRAND = math.log(COMBINATIONS, 2)
     
     # 0 0 0
     # 255 0 0
@@ -35,24 +46,18 @@ def camstream():
             for k in range (0, N_TONS):
                 color = ((TON[i], TON[j], TON[k]))
                 COLOR.append(color)
-
+    
+    MOST_DISTANCES = []
+    for i in range (0, 3):
+        MOST_DISTANCES.append((N_TONS**i)*(N_TONS-1))
+    MOST_DISTANCES.append(N_COLORS-1)
         
     display = pygame.display.set_mode(SIZE, pygame.FULLSCREEN)
+    screen = [pygame.surface.Surface(QUADRANT_SIZE, 0, display)]*4
     
-    screen1 = pygame.surface.Surface(QUADRANT_SIZE, 0, display)
-    screen2 = pygame.surface.Surface(QUADRANT_SIZE, 0, display)
-    screen3 = pygame.surface.Surface(QUADRANT_SIZE, 0, display)
-    screen4 = pygame.surface.Surface(QUADRANT_SIZE, 0, display)
-    
-    screen1.fill(COLOR[1])
-    screen2.fill(COLOR[2])
-    screen3.fill(COLOR[4])
-    screen4.fill(COLOR[7])
-    
-    display.blit(screen1, (0,0))
-    display.blit(screen2, (W_OFFSET,0))
-    display.blit(screen3, (0,H_OFFSET))
-    display.blit(screen4, (W_OFFSET,H_OFFSET))
+    for i in range(0, 4):
+        screen[i].fill(COLOR[MOST_DISTANCES[i]])
+        display.blit(screen[i], (((i&1)*W_OFFSET), ((i&2)>>1)*H_OFFSET))
     
     capture = True
     while capture:
