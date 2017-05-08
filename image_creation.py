7bfcdcfb665e412e60c8a558348ce07f6660f6ce
Thity <thierry.bossy@romandie.com>
2017-05-09 00:51:50 +0200

    working with scheme for 4/3

--- image_creation.py
+++ image_creation.py
@@ -22,6 +22,19 @@ def decode(compressed):
     string = in_bytes.decode("utf-8")
     return string
 
+def color_creation(n_tons):
+    #select the different values
+    ton = [(i)*255/(n_tons-1) for i in range(n_tons)]
+    
+    # fill the color array
+    colors = []
+    for i in range(n_tons):
+        for j in range(n_tons):
+            for k in range (n_tons):
+                color = ((ton[i], ton[j], ton[k]))
+                colors.append(color)
+    return colors
+
 def base_change(in_array, in_base, out_base):
     starting_zeros = 0
     num = 0
@@ -84,7 +97,7 @@ def display(text, rows=3, columns=5, n_tons=2, cross_size=30):
     
     # number of tiles per quadrant
     N_TILES = rows*columns
-    TILE_SIZE = (QUADRANT_SIZE[0]/columns, QUADRANT_SIZE[1]/rows)
+    TILE_SIZE = (QUADRANT_SIZE[0]//columns, QUADRANT_SIZE[1]//rows)
     TILE_OFFSETS = []
     for r in range(rows):
         for c in range(columns):
@@ -92,45 +105,42 @@ def display(text, rows=3, columns=5, n_tons=2, cross_size=30):
 
     # number of possible levels for each color
     N_TONS = n_tons
-    TON = [(i)*255/(N_TONS-1) for i in range(N_TONS)]
     # number of colors
     N_COLORS = N_TONS**3
 
     # fill the color array
-    COLOR = []
-    for i in range(N_TONS):
-        for j in range(N_TONS):
-            for k in range (N_TONS):
-                color = ((TON[i], TON[j], TON[k]))
-                COLOR.append(color)
+    COLOR = color_creation(N_TONS)
 
-    # select indexes of 4 most distances colors (red, green, blue, white)
+    # select indexes of the 4 most distanced colors (red, green, blue, white)
     MOST_DISTANCES = []
     for i in range(3):
         MOST_DISTANCES.append((N_TONS**i)*(N_TONS-1))
     MOST_DISTANCES.append(N_COLORS-1)
     
     color_message = text_to_colors(text, N_TONS)
-    print("verification text (reverse):", colors_to_text(color_message, N_TONS))
     
     #number of colors not used at the end of the message
     n_colors_last_quadrant = len(color_message) % N_TILES
     remaining_colors = (N_TILES - n_colors_last_quadrant) if n_colors_last_quadrant else 0
     #number of quadrants needed for the message
     n_quadrants = len(color_message) // N_TILES + (remaining_colors != 0)
-    print("number of quadrants:", n_quadrants)
-    print("remaining colors:", remaining_colors)
     
     #fill a color matrix where each row is a quadrant
     quadrants_colors = [color_message[N_TILES*y:N_TILES*(y+1)] for y in range(n_quadrants)]
-    print(quadrants_colors)
     
-    #create starting quadrants
-    start_quadrants = [pygame.surface.Surface(QUADRANT_SIZE) for i in range(4)]
+    print("-> Verification text (reverse):\n", colors_to_text(color_message, N_TONS))
+    print("-> Number of quadrants:", n_quadrants)
+    print("-> Remaining colors:", remaining_colors)
+    print("-> Colors of the message:\n", quadrants_colors)
+    
+    # create particular quadrants : 4 starting quadrants (red, green, blue, white) plus the black one
+    particular_quadrants = [pygame.surface.Surface(QUADRANT_SIZE) for i in range(5)]
     for i in range(4):
-        start_quadrants[i].fill(COLOR[MOST_DISTANCES[i]])
+        particular_quadrants[i].fill(COLOR[MOST_DISTANCES[i]])
+    particular_quadrants[4].fill(COLOR[0])
+
     
-    #create every quadrants for the message
+    # create every quadrants for the message
     quadrants = [pygame.surface.Surface(QUADRANT_SIZE) for i in range(n_quadrants)]
     for q in range(len(quadrants)):
         for c in range(len(quadrants_colors[q])):
@@ -140,26 +150,67 @@ def display(text, rows=3, columns=5, n_tons=2, cross_size=30):
     
     # set the display to the entire screen
     display = pygame.display.set_mode((0,0), pygame.FULLSCREEN)#, max(math.ceil(math.log(7**3, 2)), 8))
-    
     for i in range(4):
-        display.blit(start_quadrants[i], QUADRANT_OFFSETS[i])
+        display.blit(particular_quadrants[i], QUADRANT_OFFSETS[i])
 
+
+    # run with the following scheme:
+    
+    ###############################
+    ##### 1 ###### 2 ###### 3 #####
+    ###       ##       ##       ###
+    ###  D A  ##  B D  ##  C C  ###
+    ###  A A  ##  B B  ##  D C  ###
+    ###       ##       ##       ###
+    ###############################
+    ###############################
+    
     run = True
-    
+    current_quadrant = 0
     pygame.display.flip()
-    
     while run:
         for event in pygame.event.get():
             if event.type == KEYDOWN and event.key == K_s:
-                for i in range(4):#WARNING OUT OF RANGE
-                    display.blit(quadrants[random.randint(0, n_quadrants-1)], QUADRANT_OFFSETS[i])
-                    pygame.display.flip()
+                if(current_quadrant >= n_quadrants):
+                    run = False
+                    break
+                config = current_quadrant%4
+                if config == 0:
+                    for i in (1,2,3):
+                        display.blit(quadrants[current_quadrant], QUADRANT_OFFSETS[i])
+                    if(current_quadrant+3 < n_quadrants):
+                        display.blit(quadrants[current_quadrant+3], QUADRANT_OFFSETS[0])
+                    else:
+                        #fill in black if nothin to display
+                        display.blit(particular_quadrants[4], QUADRANT_OFFSETS[0])
+                elif config == 1:
+                    for i in (0,2,3):
+                        display.blit(quadrants[current_quadrant], QUADRANT_OFFSETS[i])
+                    if(current_quadrant+2 < n_quadrants):
+                        display.blit(quadrants[current_quadrant+2], QUADRANT_OFFSETS[1])
+                    else:
+                        #fill in black if nothin to display
+                        display.blit(particular_quadrants[4], QUADRANT_OFFSETS[1])
+                elif config == 2:
+                    for i in (0,1,3):
+                        display.blit(quadrants[current_quadrant], QUADRANT_OFFSETS[i])
+                    if(current_quadrant+1 < n_quadrants):
+                        display.blit(quadrants[current_quadrant+1], QUADRANT_OFFSETS[2])
+                    else:
+                        #fill in black if nothin to display
+                        display.blit(particular_quadrants[4], QUADRANT_OFFSETS[2])
+                    # at config 2, one more increment to skip the D
+                    current_quadrant += 1
+                else:
+                    print("ERROR: didn't skip a fourth screen of cycle")
+                  
+                pygame.display.flip()
+                current_quadrant += 1
             if event.type == KEYDOWN and event.key == K_q:
                 run = False
 
     pygame.quit()
     return
-
 
 text200 = """In the midst of the Great Desert lived two tribes of people.
 Both were nomadic in nature and continuously traveled across the arid landscape,
@@ -179,4 +230,4 @@ And there in the middle flowed a deep spring of crystalline waters.
 When the sultan of the tribe beheld this jewel of the..."""
 
 #(text, rows=3, columns=5, n_tons=2, cross_size=30)
-display(text1000, 3, 5, 2)
+display(text200, 3, 5, 3)
