ceec094eeb1c7ef49323218d6de9d059809eb592
Thity <thierry.bossy@romandie.com>
2017-05-19 16:40:30 +0200

    scheme improvements

--- image_creation.py
+++ image_creation.py
@@ -119,19 +119,14 @@ def display(text, rows=3, columns=5, n_tons=2, refresh_interval=110, cross_size=
     
     color_message = text_to_colors(text, N_TONS)
     
-    #number of colors not used at the end of the message
+    # number of colors not used at the end of the message
     n_colors_last_quadrant = len(color_message) % N_TILES
     remaining_colors = (N_TILES - n_colors_last_quadrant) if n_colors_last_quadrant else 0
-    #number of quadrants needed for the message
-    n_quadrants = len(color_message) // N_TILES + (remaining_colors != 0)
+    # number of quadrants needed for the message
+    n_message_quadrants = len(color_message) // N_TILES + (remaining_colors != 0)
     
-    #fill a color matrix where each row is a quadrant
-    quadrants_colors = [color_message[N_TILES*y:N_TILES*(y+1)] for y in range(n_quadrants)]
-    
-    print("-> Verification text (reverse):\n", colors_to_text(color_message, N_TONS))
-    print("-> Number of quadrants:", n_quadrants)
-    print("-> Remaining colors:", remaining_colors)
-    print("-> Colors of the message:\n", quadrants_colors)
+    # fill a color matrix where each row is a quadrant
+    quadrants_colors = [color_message[N_TILES*y:N_TILES*(y+1)] for y in range(n_message_quadrants)]
     
     # create particular quadrants : 4 starting quadrants (red, green, blue)
     # plus yellow to avoid white plus the black one
@@ -142,18 +137,62 @@ def display(text, rows=3, columns=5, n_tons=2, refresh_interval=110, cross_size=
     particular_quadrants[3].fill(COLOR[yellow_index]) #yellow
     particular_quadrants[4].fill(COLOR[0]) #black
     
+    # create the dictionnary and padding quadrant with the colors, and the number of remaining colors after
+    max_tiles_for_padding = math.ceil(math.log(N_TILES, N_COLORS))
+    
+    # number of tiles and quadrants needed for dictionnary and padding
+    max_tiles_for_dpq = N_COLORS + max_tiles_for_padding
+    n_dictionnary_padding_quadrant = max_tiles_for_dpq // N_TILES + ((max_tiles_for_dpq%N_TILES) != 0)
+    
+    # sequence of colors to send the number of padding colors
+    colors_for_padding = base_change([remaining_colors], 10, n_colors)
+    
+    # real number of tiles for padding in function of the message
+    n_tiles_for_padding = len(colors_for_padding)
+    n_tiles_for_dpq = N_COLORS + n_tiles_for_padding
+    
+    # sequence of colors for the dictionnary and padding
+    colors_for_dictionnary_padding = list(range(N_COLORS)) + colors_for_padding
+    
+    # creates the dictionnary padding quadrants and fill the in black
+    dictionnary_padding_quadrants = [pygame.surface.Surface(QUADRANT_SIZE) for i in range(n_dictionnary_padding_quadrant)]
+    for i in range(n_dictionnary_padding_quadrant):
+        dictionnary_padding_quadrants[i].fill(COLOR[0])
+    
+    # fill the dictionnary padding quadrants with the needed colors
+    for c in range(n_tiles_for_dpq):
+        quadrant_index = c // N_TILES
+        tile_index = c % N_TILES
+        color = COLOR[colors_for_dictionnary_padding[c]]
+        rect = pygame.Rect(TILE_OFFSETS[tile_index], TILE_SIZE)
+        dictionnary_padding_quadrants[quadrant_index].fill(color, rect)
+
     # create every quadrants for the message
-    quadrants = [pygame.surface.Surface(QUADRANT_SIZE) for i in range(n_quadrants)]
-    for q in range(len(quadrants)):
+    message_quadrants = [pygame.surface.Surface(QUADRANT_SIZE) for i in range(n_message_quadrants)]
+    for q in range(len(message_quadrants)):
         for c in range(len(quadrants_colors[q])):
             color = COLOR[quadrants_colors[q][c]]
             rect = pygame.Rect(TILE_OFFSETS[c], TILE_SIZE)
-            quadrants[q].fill(color, rect)
+            message_quadrants[q].fill(color, rect)
+            
+    # put the dictionnary_padding as the first quadrant of the message
+    n_quadrants = n_dictionnary_padding_quadrant + n_message_quadrants
+    quadrants = dictionnary_padding_quadrants + message_quadrants
     
     # set the display to the entire screen
     display = pygame.display.set_mode((0,0), pygame.FULLSCREEN)
     for i in range(4):
         display.blit(particular_quadrants[i], QUADRANT_OFFSETS[i])
+    
+    
+    # Printing informations ----------------------------------------------------------
+    print("-> Colors of the message:\n", quadrants_colors)
+    print("-> Verification text (reverse):\n", colors_to_text(color_message, N_TONS))
+    print("-> Number of quadrants:", n_quadrants)
+    print("-> Remaining colors:", remaining_colors)
+    print("-> Number of tiles for padding:", n_tiles_for_padding)
+    print("-> Number of quadrants for dictionnary and padding:", n_dictionnary_padding_quadrant)
+    # --------------------------------------------------------------------------------
 
 
     # run with the following scheme:
@@ -182,9 +221,9 @@ def display(text, rows=3, columns=5, n_tons=2, refresh_interval=110, cross_size=
             # Loop and refresh screen
             if event.type == USEREVENT:
                 if(current_quadrant >= n_quadrants):
-                    # fill everything in black at the end
+                    # fill everything in green at the end
                     for i in range(4):
-                        display.blit(particular_quadrants[4], QUADRANT_OFFSETS[i])
+                        display.blit(particular_quadrants[1], QUADRANT_OFFSETS[i])
                     pygame.display.flip()
                     break
                 config = current_quadrant%4
@@ -194,24 +233,24 @@ def display(text, rows=3, columns=5, n_tons=2, refresh_interval=110, cross_size=
                     if(current_quadrant+3 < n_quadrants):
                         display.blit(quadrants[current_quadrant+3], QUADRANT_OFFSETS[0])
                     else:
-                        # fill in black if nothin to display
-                        display.blit(particular_quadrants[4], QUADRANT_OFFSETS[0])
+                        # fill in green if nothin to display
+                        display.blit(particular_quadrants[1], QUADRANT_OFFSETS[0])
                 elif config == 1:
                     for i in (0,2,3):
                         display.blit(quadrants[current_quadrant], QUADRANT_OFFSETS[i])
                     if(current_quadrant+2 < n_quadrants):
                         display.blit(quadrants[current_quadrant+2], QUADRANT_OFFSETS[1])
                     else:
-                        # fill in black if nothin to display
-                        display.blit(particular_quadrants[4], QUADRANT_OFFSETS[1])
+                        # fill in green if nothin to display
+                        display.blit(particular_quadrants[1], QUADRANT_OFFSETS[1])
                 elif config == 2:
                     for i in (0,1,3):
                         display.blit(quadrants[current_quadrant], QUADRANT_OFFSETS[i])
                     if(current_quadrant+1 < n_quadrants):
                         display.blit(quadrants[current_quadrant+1], QUADRANT_OFFSETS[2])
                     else:
-                        # fill in black if nothin to display
-                        display.blit(particular_quadrants[4], QUADRANT_OFFSETS[2])
+                        # fill in green if nothin to display
+                        display.blit(particular_quadrants[1], QUADRANT_OFFSETS[2])
                     # at config 2, one more increment to skip the D
                     current_quadrant += 1
                 else:
@@ -244,4 +283,4 @@ And there in the middle flowed a deep spring of crystalline waters.
 When the sultan of the tribe beheld this jewel of the..."""
 
 #(text, rows=3, columns=5, n_tons=2, refresh_interval=500, cross_size=30)
-display(text1000, 3, 5, 2, 110)
+display(text1000, 3, 5, 2, 5000)
--- image_decoding.py
+++ image_decoding.py
@@ -3,42 +3,43 @@ from crop import*
 import numpy as np
 import os
 
-# return the closest color which is in our color set from a given detected color
-def closest_color(detected_color, n_tons):
+# return the closest color which is in our color set from a given detected color.
+# If 2 tons, use a threshold more accurate than closest_color
+def closest_color(detected_color, n_tons, threshold=220):
     delta = 256//(2*(n_tons-1))
-    r = ((int(detected_color[0])+delta)*(n_tons-1))//255
-    g = ((int(detected_color[1])+delta)*(n_tons-1))//255
-    b = ((int(detected_color[2])+delta)*(n_tons-1))//255
+    r = detected_color[0] > threshold if n_tons == 2 else ((int(round(detected_color[0]))+delta)*(n_tons-1))//255
+    g = detected_color[1] > threshold if n_tons == 2 else ((int(round(detected_color[1]))+delta)*(n_tons-1))//255
+    b = detected_color[2] > threshold if n_tons == 2 else ((int(round(detected_color[2])+delta))*(n_tons-1))//255
     
     color_index = r*n_tons**2 + g*n_tons + b
     return color_index
 
 # decode_box takes a box, which is made up of paritions, and
 # returns the average (R, G, B) vector for each partition.
-def decode_box(image, box, n_tones):
+def decode_box(image, box, n_tones, delta):
     n_box = len(box)
     
     tone_list = []
     for (top, bottom) in box:
         mean_tone = np.array([0, 0, 0])
-        normalise = 1.0 / ((bottom[1] - top[1]) * (bottom[0] - top[0])) *1.0
-        for i in range(top[1], bottom[1]):
-            for j in range(top[0], bottom[0]):
+        normalise = 1.0 / ((bottom[1] - top[1] - 2*delta) * (bottom[0] - top[0] - 2*delta))
+        for i in range(top[1]+delta, bottom[1]-delta):
+            for j in range(top[0]+delta, bottom[0]-delta):
                 mean_tone += image[i][j]
 
         tone_list.append(closest_color(mean_tone*normalise, n_tones))
-        # tone_list.append(mean_tone * normalise)
+        #tone_list.append(np.rint(mean_tone*normalise))
 
     return tone_list
 
 
 # decode_image takes a list of boxes and returns the corresponding
 # decode_box for each box
-def decode_image(image, boxes, n_tones):
+def decode_image(image, boxes, n_tones, delta=0):
     box_tone_list = []
 
     for box in boxes:
-        box_tone_list.append(decode_box(image, box, n_tones))
+        box_tone_list.append(decode_box(image, box, n_tones, delta))
     return box_tone_list
 
 
@@ -49,29 +50,36 @@ def decode_image(image, boxes, n_tones):
 file_name = os.path.join(os.path.dirname(__file__), 'Captures/shots1/pic2.png')
 
 # Image with some message (i.e. screen partition encodings)
-file_name2 = os.path.join(os.path.dirname(__file__), 'Captures/shots1/pic12.png')
+file_name2 = os.path.join(os.path.dirname(__file__), 'Captures/shots1/pic9.png')
 
 img = Image.open(file_name)
 img2 = Image.open(file_name2)
 
 # Output values
-output_name = "crop_output_test1.png"
-corner_output="corner_output"
-
+output_path = os.path.join(os.path.dirname(__file__), 'Captures/shots1/cropped/')
 
 arr = np.array(img)
 dim = arr.shape;
 
 arr2 = np.array(img2)
 
-
-source_img = img.convert("RGBA")
-draw = ImageDraw.Draw(source_img)
-
 v_part, h_part = 3, 5
 
 partitions = get_unit_crop_coorinates(arr, dim, v_part, h_part)
-decoded = decode_image(arr2, partitions, 2)
+
+##### TESTING
+for i in range(len(partitions)):
+    for j in range(len(partitions[i])):
+        left = partitions[i][j][0][0]+3
+        upper = partitions[i][j][0][1]+3
+        right = partitions[i][j][1][0]-3
+        lower = partitions[i][j][1][1]-3
+        croped = img2.crop((left, upper, right, lower))
+        croped.save(output_path + str(i) + str(j) + ".png")
+##### END OF TESTING
+
+
+decoded = decode_image(arr2, partitions, 2, 3)
 
 for index in decoded:
     print(index)
